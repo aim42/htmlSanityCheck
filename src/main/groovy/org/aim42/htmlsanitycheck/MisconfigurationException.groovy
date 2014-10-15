d193afc53c37bac60b5e5aa09d51a71892a3453e
Dr. Gernot Starke <gs@gernotstarke.de>
2014-10-15 16:41:09 +0300

    added a specific Exception type to communicate misconfigurations

---
+++ src/main/groovy/org/aim42/htmlsanitycheck/MisconfigurationException.groovy
@@ -0,0 +1,10 @@
+package org.aim42.htmlsanitycheck
+
+// see end-of-file for license information
+
+
+class MisconfigurationException {
+    MisconfigurationException(String message) {
+        super(message)
+    }
+}
