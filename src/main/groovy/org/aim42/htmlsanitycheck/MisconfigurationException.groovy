123a6ef9c5491376d241eb1920d291e3de6596ce
Dr. Gernot Starke <gs@gernotstarke.de>
2014-10-16 14:17:15 +0300

    enhanced Misconfiguration exception

--- src/main/groovy/org/aim42/htmlsanitycheck/MisconfigurationException.groovy
+++ src/main/groovy/org/aim42/htmlsanitycheck/MisconfigurationException.groovy
@@ -3,8 +3,14 @@ package org.aim42.htmlsanitycheck
 // see end-of-file for license information
 
 
-class MisconfigurationException {
-    MisconfigurationException(String message) {
+class MisconfigurationException extends Exception {
+
+
+    public MisconfigurationException( String message, File srcDir) {
+        super( message + ": " + srcDir.canonicalPath )
+    }
+
+    public MisconfigurationException( String message ) {
         super(message)
     }
 }
