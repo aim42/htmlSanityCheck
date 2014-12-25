68c66f2baeada75044879be25b13718b676f5725
gernots <gernot.starke@innoq.com>
2014-12-25 19:11:05 +0100

    refrained from testing positive cases (that is, correct local references)

--- core/src/test/groovy/org/aim42/htmlsanitycheck/check/MissingLocalResourceRefCountSpec.groovy
+++ core/src/test/groovy/org/aim42/htmlsanitycheck/check/MissingLocalResourceRefCountSpec.groovy
@@ -1,5 +1,6 @@
 package org.aim42.htmlsanitycheck.check
 
+import org.aim42.htmlsanitycheck.collect.SingleCheckResults
 import org.aim42.htmlsanitycheck.html.HtmlConst
 import org.aim42.htmlsanitycheck.html.HtmlPage
 import spock.lang.Specification
@@ -7,12 +8,15 @@ import spock.lang.Unroll
 
 class MissingLocalResourceRefCountSpec extends Specification {
 
-    private static final String LOCAL_RESOURCE_NAME = "ExistingLocalResource.html"
-
     // missing local resource name
     private static final String MIS_LOC_RES = "MissingLocalResource.html"
 
     private static final String badLocalRef =  """<a href="${MIS_LOC_RES}">missing</a>"""
+
+
+    private static final String msgPrefix = MissingLocalResourcesChecker.MLRC_MESSAGE_PREFIX
+    private static final String msgMissing = MissingLocalResourcesChecker.MLRC_MESSAGE_MISSING
+    private static final String msgRefCount = MissingLocalResourcesChecker.MLRC_REFCOUNT
 
 
     private File tmpDir
@@ -24,7 +28,7 @@ class MissingLocalResourceRefCountSpec extends Specification {
      * data-driven test to specify behavior of MissingLocalResourceChecker reference counter:
      * if a single local resource is referenced multiple times,
      * it will only result in a single line of output of the following form:
-     * "local resource <name> missing missing (reference count 5)"
+     * "local resource <name> missing missing, reference count: 5"
      *
      * @param htmlsnippet
      * @param nrOfChecks
@@ -33,47 +37,50 @@ class MissingLocalResourceRefCountSpec extends Specification {
      */
 
     @Unroll
-    def "MLR checker has reference counter"(int nrOfFindings,
-                                            int nrOfChecks,
+    def "MLR checker has reference counter"(int nrOfChecks,
+                                            int nrOfFindings,
                                             String htmlSnippet,
                                             String result) {
 
         given:
 
         String html = HtmlConst.HTML_HEAD + htmlSnippet + HtmlConst.HTML_END
-        HtmlPage htmlPage = new HtmlPage(html)
 
-        (tmpHtmlFile, tmpDir, tmpLocalResourceFile) = createTempLocalResources("index.html", html, LOCAL_RESOURCE_NAME)
+        // create a file
+        (tmpHtmlFile, tmpDir) = createTempLocalResources("index.html", html)
+
+        HtmlPage htmlPage = new HtmlPage( tmpHtmlFile )
 
         when:
         def missingLocalResourcesChecker = new MissingLocalResourcesChecker(
                 pageToCheck: htmlPage,
                 baseDirPath: tmpDir )
-        def collector = missingLocalResourcesChecker.performCheck()
+        SingleCheckResults collector = missingLocalResourcesChecker.performCheck()
 
 
         then:
 
+        println "testing with $nrOfChecks, $nrOfFindings, $htmlSnippet, expected: $result "
+
         // our temporary  files still exists
         tmpHtmlFile.exists()
-        tmpLocalResourceFile.exists()
 
-
-        collector.nrOfProblems() == nrOfFindings
+        // we get the correct nr of checks and findings
         collector.nrOfItemsChecked == nrOfChecks
+        collector.nrOfProblems() == nrOfFindings
 
-        if (nrOfFindings > 0) {
-            // we specify only the FIRST message
-            collector.findings.first().item == result
-        }
+        // we get the correct finding-message
+        collector.findings.first().item == result
 
 
         where:
 
-        nrOfFindings | nrOfChecks | htmlSnippet | result
-        0 | 1 | """<a href="./${LOCAL_RESOURCE_NAME}">existing</a>"""|  """"""
-        1 | 1 | badLocalRef | """local resource ${MIS_LOC_RES} missing"""
-        2 | 2 | badLocalRef*2 | """local resource ${MIS_LOC_RES} missing"""
+        nrOfChecks | nrOfFindings | htmlSnippet | result
+        // one bad local reference:
+        1 | 1 | badLocalRef | """${msgPrefix} "${MIS_LOC_RES}" ${msgMissing}"""
+
+        // two bad local references to the same file -> reference count == 2
+        2 | 2 | badLocalRef*2 | """${msgPrefix} ${MIS_LOC_RES} ${msgMissing}${msgRefCount} 2"""
 
     }
 
@@ -81,22 +88,16 @@ class MissingLocalResourceRefCountSpec extends Specification {
     * helper to create local resource
      */
 
-    def List createTempLocalResources(String htmlFileName, String htmlContent, String localResourceName) {
+    def List createTempLocalResources(String htmlFileName, String htmlContent) {
         // 1.) create tmp directory tmpDir
         File tmpDir = File.createTempDir()
 
-        // 2.) create local resource file localResourceFile
-        File localResourceFile = new File(tmpDir, localResourceName) << "someContent"
-
-        // make sure the just-created (local resource) file really exists
-        assert localResourceFile.exists()
-
-        // 3.) create tmp html file "index.html" linking to localResourceFile in directory tmpDir
+        // 2.) create tmp html file "index.html" linking to localResourceFile in directory tmpDir
         File indexFile = new File(tmpDir, htmlFileName) << htmlContent
 
         assert indexFile.exists()
 
-        return [indexFile, tmpDir, localResourceFile]
+        return [indexFile, tmpDir]
     }
 }
 
