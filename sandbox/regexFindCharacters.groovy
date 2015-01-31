94aa5861807a256d456dbd13ba267fd757c5d79f
gernots <gernot.starke@innoq.com>
2015-01-31 21:55:24 +0100

    more refinement

--- sandbox/regexFindCharacters.groovy
+++ sandbox/regexFindCharacters.groovy
@@ -10,15 +10,20 @@ assert containsInvalidChars( "#Context-Analysis" ) == false
 assert containsInvalidChars( "#Context_Analysis" ) == false
 
 
-def boolean containsInvalidChars( String aString ) {
+public static boolean containsInvalidChars( String aString ) {
    String illegalCharsRegex = / |!/
 
-   if (!(aString =~ illegalCharsRegex)) {
-      println "chars not found"
+   print """\"$aString\" """ 
+   
+   def m = (aString =~ illegalCharsRegex)
+   // assert m instanceof Matcher
+   
+   if (!(m.find())) {
+      println "does NOT contain illegal chars."
       return false
    }
    else { 
-      println "found"
+      println "contains illegal chars."
       return true
    }   
 }
