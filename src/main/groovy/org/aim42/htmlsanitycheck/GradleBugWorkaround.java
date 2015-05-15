3d2f555c16ec37cb2e2ccc48fc8ddcc3dda44990
gernots <gernot.starke@innoq.com>
2015-05-15 12:51:50 +0200

    unmodularized... moved all sources back into single gradle project

---
+++ src/main/groovy/org/aim42/htmlsanitycheck/GradleBugWorkaround.java
@@ -0,0 +1,12 @@
+package org.aim42.htmlsanitycheck;
+
+/**
+* gradle 2.1 has a nasty javadoc bug.
+
+(gradle issue #3169), remove this class when this issue is fixed.
+**/
+
+public class GradleBugWorkaround {
+	int i;
+
+}
