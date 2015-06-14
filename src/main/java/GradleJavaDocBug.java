6f426f2673f9a7ae1e004002662798855d84f93b
gernots <gernot.starke@innoq.com>
2015-06-14 17:31:07 +0200

    worked on #39, released first version to gradle plugin portal

---
+++ src/main/java/GradleJavaDocBug.java
@@ -0,0 +1,30 @@
+/**
+ * here's for some unused JavaDoc.
+ * We need this class to work around a Gradle Bug...
+ */
+public class GradleJavaDocBug {
+  int i;
+
+}
+
+/************************************************************************
+ * This is free software - without ANY guarantee!
+ * <p/>
+ * <p/>
+ * Copyright Dr. Gernot Starke, arc42.org
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p/>
+ * **********************************************************************
+ */
+
