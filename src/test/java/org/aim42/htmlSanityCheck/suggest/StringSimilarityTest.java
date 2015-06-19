cb66c04ff3c42e2410bfb3606c3634735e733a8a
gernots <gernot.starke@innoq.com>
2015-06-19 16:54:38 +0200

    tested findTop (most similar string) with short and long list...

--- src/test/java/org/aim42/htmlSanityCheck/suggest/StringSimilarityTest.java
+++ src/test/java/org/aim42/htmlSanityCheck/suggest/StringSimilarityTest.java
@@ -1,9 +1,11 @@
 package org.aim42.htmlSanityCheck.suggest;
 
 import net.ricecode.similarity.*;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -15,9 +17,20 @@ import static org.junit.Assert.assertEquals;
 
 public class StringSimilarityTest {
 
+    SimilarityStrategy strategy;
+    StringSimilarityService service;
+    List<String> features;
+
+    @Before
+    public void setup() {
+        strategy = new JaroWinklerStrategy();
+        service = new StringSimilarityServiceImpl(strategy);
+
+        features = new ArrayList<String>();
+    }
+
     @Test
-    public void testScoreAll() {
-        SimilarityStrategy strategy = new JaroWinklerStrategy();
+    public void testFindTopInShortList() {
         String target = "McDonalds";
 
         String c1 = "MacMahons";
@@ -25,8 +38,6 @@ public class StringSimilarityTest {
         String c3 = "McDonaldz";
 
 
-        StringSimilarityService service = new StringSimilarityServiceImpl(strategy);
-        List<String> features = new ArrayList<String>();
         features.add(c1);
         features.add(c2);
         features.add(c3);
@@ -38,95 +49,68 @@ public class StringSimilarityTest {
         assertEquals( "McDonaldz is expected to be the best match", "McDonaldz",
                 service.findTop(features, target).getKey());
 
-
     }
 
 
-//
-//	@Test
-//	public void testFindTop() {
-//		SimilarityStrategy strategy = mock(SimilarityStrategy.class);
-//		String target = "McDonalds";
-//		String c1 = "MacMahons";
-//		String c2 = "McPherson";
-//		String c3 = "McDonalds";
-//
-//		SimilarityScore expected = new SimilarityScore(c3, 1.000);
-//
-//		when(strategy.score(c1, target)).thenReturn(0.90);
-//		when(strategy.score(c2, target)).thenReturn(0.74);
-//		when(strategy.score(c3, target)).thenReturn(1.000);
-//
-//		StringSimilarityService service = new StringSimilarityServiceImpl(strategy);
-//		List<String> features = new ArrayList<String>();
-//		features.add(c1);
-//		features.add(c2);
-//		features.add(c3);
-//
-//		SimilarityScore top= service.findTop(features,target);
-//		verify(strategy).score(c1, target);
-//		verify(strategy).score(c2, target);
-//		verify(strategy).score(c3, target);
-//		assertEquals(expected, top);
-//
-//	}
-//
-//	@Test
-//	public void testFindTop_Ascending() {
-//		SimilarityStrategy strategy = mock(SimilarityStrategy.class);
-//		String target = "McDonalds";
-//		String c1 = "MacMahons";
-//		String c2 = "McPherson";
-//		String c3 = "McDonalds";
-//
-//		SimilarityScore expected = new SimilarityScore(c2, 0.74);
-//
-//		when(strategy.score(c1, target)).thenReturn(0.90);
-//		when(strategy.score(c2, target)).thenReturn(0.74);
-//		when(strategy.score(c3, target)).thenReturn(1.000);
-//
-//		StringSimilarityService service = new StringSimilarityServiceImpl(strategy);
-//		List<String> features = new ArrayList<String>();
-//		features.add(c1);
-//		features.add(c2);
-//		features.add(c3);
-//
-//		AscendingSimilarityScoreComparator comparator = new AscendingSimilarityScoreComparator();
-//		SimilarityScore top= service.findTop(features,target, comparator);
-//		verify(strategy).score(c1, target);
-//		verify(strategy).score(c2, target);
-//		verify(strategy).score(c3, target);
-//		assertEquals(expected, top);
-//	}
-//
-//	@Test
-//	public void testFindTop_Descending() {
-//		SimilarityStrategy strategy = mock(SimilarityStrategy.class);
-//		String target = "McDonalds";
-//		String c1 = "MacMahons";
-//		String c2 = "McPherson";
-//		String c3 = "McDonalds";
-//
-//		SimilarityScore expected = new SimilarityScore(c3, 1.000);
-//
-//		when(strategy.score(c1, target)).thenReturn(0.90);
-//		when(strategy.score(c2, target)).thenReturn(0.74);
-//		when(strategy.score(c3, target)).thenReturn(1.000);
-//
-//		StringSimilarityService service = new StringSimilarityServiceImpl(strategy);
-//		List<String> features = new ArrayList<String>();
-//		features.add(c1);
-//		features.add(c2);
-//		features.add(c3);
-//
-//		DescendingSimilarityScoreComparator comparator = new DescendingSimilarityScoreComparator();
-//		SimilarityScore top= service.findTop(features,target, comparator);
-//		verify(strategy).score(c1, target);
-//		verify(strategy).score(c2, target);
-//		verify(strategy).score(c3, target);
-//		assertEquals(expected, top);
-//
-//	}
+    @Test
+    public void testFindSuggestionInLongList() {
+        String target = "Bröncohr";
+        String bestMatch = "Broncor";
+
+        features = Arrays.asList(
+                "Abomasnow", "Abra", "Absol", "Accelgor", "Aerodactyl", "Aggron",
+                "Aipom", "Alakazam", "Alomomola", "Altaria", "Ambipom", "Amoonguss",
+                "Ampharos", "Anorith", "Arbok", "Arcanine", "Arceus", "Archen", "Archeops",
+                "Ariados", "Armaldo", "Aron", "Articuno", "Audino", "Axew", "Azelf",
+                "Azumarill", "Azurill", "Bagon", "Baltoy", "Banette", "Barboach",
+                "Basculin", "Bastiodon", "Bayleef", "Beartic", "Beautifly", "Beedrill",
+                "Beheeyem", "Beldum", "Bellossom", "Bellsprout", "Bibarel", "Bidoof",
+                "Bisharp", "Blastoise", "Blaziken", "Blissey", "Blitzle", "Boldore",
+                "Bonsly", "Bouffalant", "Braviary", "Breloom", "Bronzong", bestMatch,
+                "Budew", "Buizel", "Bulbasaur", "Buneary", "Burmy", "Butterfree", "Cacnea",
+                "Cacturne", "Camerupt", "Carnivine", "Carracosta", "Carvanha", "Cascoon",
+                "Castform", "Caterpie", "Celebi", "Chandelure", "Chansey", "Charizard",
+                "Charmander", "Charmeleon", "Chatot", "Cherrim", "Cherubi", "Chikorita",
+                "Chimchar", "Chimecho", "Chinchou", "Chingling", "Cinccino", "Clamperl",
+                "Claydol", "Clefable", "Clefairy", "Cleffa", "Cloyster", "Cobalion",
+                "Cofagrigus", "Combee", "Combusken", "Conkeldurr", "Corphish", "Corsola",
+                "Cottonee", "Cradily", "Cranidos", "Crawdaunt", "Cresselia", "Croagunk",
+                "Crobat", "Croconaw", "Crustle", "Cryogonal", "Cubchoo", "Cubone",
+                "Cyndaquil", "Darkrai", "Darmanitan", "Darumaka", "Deerling", "Deino",
+                "Delcatty", "Delibird", "Deoxys", "Dewgong", "Dewott", "Dialga", "Diglett",
+                "Ditto", "Dodrio", "Doduo", "Donphan", "Dragonair", "Dragonite", "Drapion",
+                "Dratini", "Drifblim", "Drifloon", "Drilbur", "Drowzee", "Druddigon",
+                "Ducklett", "Dugtrio", "Dunsparce", "Duosion", "Durant", "Dusclops",
+                "Dusknoir", "Duskull", "Dustox", "Dwebble", "Eelektrik", "Eelektross",
+                "Eevee", "Ekans", "Electabuzz", "Electivire", "Electrike", "Electrode",
+                "Elekid", "Elgyem", "Emboar", "Emolga", "Empoleon", "Entei", "Escavalier",
+                "Espeon", "Excadrill", "Exeggcute", "Exeggutor", "Exploud", "Farfetch'd",
+                "Fearow", "Feebas", "Feraligatr", "Ferroseed", "Ferrothorn", "Finneon",
+                "Flaaffy", "Flareon", "Floatzel", "Flygon", "Foongus", "Forretress",
+                "Fraxure", "Frillish", "Froslass", "Furret", "Gabite", "Gallade",
+                "Galvantula", "Garbodor", "Garchomp", "Gardevoir", "Gastly", "Gastrodon",
+                "Gengar", "Geodude", "Gible", "Gigalith", "Girafarig", "Giratina", "Glaceon",
+                "Glalie", "Glameow", "Gligar", "Gliscor", "Gloom", "Golbat", "Goldeen",
+                "Golduck", "Golem", "Golett", "Golurk", "Gorebyss", "Gothita", "Gothitelle",
+                "Gothorita", "Granbull", "Graveler", "Grimer", "Grotle", "Groudon",
+                "Grovyle", "Growlithe", "Grumpig", "Gulpin", "Gurdurr", "Gyarados",
+                "Happiny", "Hariyama", "Haunter", "Haxorus", "Heatmor", "Heatran",
+                "Heracross", "Herdier", "Hippopotas", "Hippowdon", "Hitmonchan",
+                "Hitmonlee", "Hitmontop", "Ho-Oh", "Honchkrow", "Hoothoot", "Hoppip",
+                "Horsea", "Houndoom", "Houndour", "Huntail", "Hydreigon", "Hypno",
+                "Igglybuff", "Illumise", "Infernape", "Ivysaur", "Jellicent", "Jigglypuff",
+                "Jirachi", "Jolteon", "Joltik", "Jumpluff", "Jynx", "Kabuto", "Kabutops",
+                "Kadabra", "Kakuna", "Kangaskhan", "Karrablast", "Kecleon", "Kingdra"
+                );
+
+        List<SimilarityScore> scores = service.scoreAll(features, target);
+
+        assertEquals(267, scores.size());
+
+        assertEquals( bestMatch + " is expected to be the best match", bestMatch,
+                service.findTop(features, target).getKey());
+
+    }
 }
 
 
