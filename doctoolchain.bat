700834941d7061fdbdf7f7182e474febdd24a856
Ralf D. Müller <ralf.d.mueller@gmail.com>
2019-01-15 17:09:09 +0100

    added script for doctoolchain on powershell and cmd

---
+++ doctoolchain.bat
@@ -0,0 +1,1 @@
+docker run --rm --entrypoint /bin/bash -it -v %cd%:/project rdmueller/doctoolchain:rc-1.0.0 -c "doctoolchain . %1 %2 %3 %4 %5 %6 %7 %8 %9 -PmainConfigFile=config/docToolchain.groovy && exit"
