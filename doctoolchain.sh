16c0a9f4a953ee4f3d1a8f34ffbe7e93e12976d0
Ralf D. Müller <ralf.d.mueller@gmail.com>
2019-01-15 15:41:46 +0100

    updated script to fit container version

--- doctoolchain.sh
+++ doctoolchain.sh
@@ -1,4 +1,4 @@
-docker run --rm -it -v ${PWD}:/project rdmueller/doctoolchain:snapshot-0.1.10 \
+docker run --rm --entrypoint /bin/bash -it -v ${PWD}:/project rdmueller/doctoolchain:rc-1.0.0 \
 -c "doctoolchain . $1 $2 $3 $4 $5 $6 $7 $8 $9 -PmainConfigFile=config/docToolchain.groovy && exit"
 
 
