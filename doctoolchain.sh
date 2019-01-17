136b0855bc85e7f920934714add8ce6559ca76ea
Ralf D. Müller <ralf.d.mueller@gmail.com>
2019-01-17 18:05:35 +0100

    updated build to match docToolchain container use

--- doctoolchain.sh
+++ doctoolchain.sh
@@ -1,4 +1,4 @@
-docker run --rm --entrypoint /bin/bash -it -v ${PWD}:/project rdmueller/doctoolchain:rc-1.0.0 \
+docker run --rm --entrypoint /bin/bash  -v ${PWD}:/project rdmueller/doctoolchain:rc-1.0.0 \
 -c "doctoolchain . $1 $2 $3 $4 $5 $6 $7 $8 $9 -PmainConfigFile=config/docToolchain.groovy && exit"
 
 
