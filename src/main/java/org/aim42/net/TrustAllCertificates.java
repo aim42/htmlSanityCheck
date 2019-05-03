e0d2a7c5b5156da5257ffb827244fc7a016b3e3c
Gernot Starke <gs@gernotstarke.de>
2019-05-03 14:25:15 +0100

    fixed #272 by adding an (insecure) always-trusting TrustManager

---
+++ src/main/java/org/aim42/net/TrustAllCertificates.java
@@ -0,0 +1,60 @@
+package org.aim42.net;
+
+// created by https://www.geekality.net/2013/09/27/java-ignore-ssl-certificate-errors/
+
+import javax.net.ssl.*;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+public final class TrustAllCertificates implements X509TrustManager, HostnameVerifier
+{
+    public X509Certificate[] getAcceptedIssuers() {return null;}
+    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+    public boolean verify(String hostname, SSLSession session) {return true;}
+
+    /**
+     * Installs a new {@link TrustAllCertificates} as trust manager and hostname verifier.
+     */
+    public static void install()
+    {
+        try
+        {
+            TrustAllCertificates trustAll = new TrustAllCertificates();
+
+            // Install the all-trusting trust manager
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null,
+                    new TrustManager[]{trustAll},
+                    new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            // Install the all-trusting host verifier
+            HttpsURLConnection.setDefaultHostnameVerifier(trustAll);
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            throw new RuntimeException("Failed setting up all thrusting certificate manager.", e);
+        }
+        catch (KeyManagementException e)
+        {
+            throw new RuntimeException("Failed setting up all thrusting certificate manager.", e);
+        }
+    }
+}
+
+/*=============================================================
+  Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an
+ "AS IS" BASIS,WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ =============================================================*/
