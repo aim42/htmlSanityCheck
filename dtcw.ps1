15b5f9ccfcb033e1df6306549b1fc6e2fd8c7cbe
Ralf D. Müller <ralf.d.mueller@gmail.com>
2021-10-19 18:53:24 +0200

    set version to 2.0.2

--- dtcw.ps1
+++ dtcw.ps1
@@ -1,8 +1,8 @@
 
 $main_config_file = "docToolchainConfig.groovy"
 # $version=ng
-$version = "2.0.0-dev"
-$dockerVersion = "2.0.1"
+$version = "2.0.2"
+$dockerVersion = "2.0.2"
 $distribution_url = "https://github.com/docToolchain/docToolchain/releases/download/v$version/docToolchain-$version.zip"
 
 $dtc_opts="$dtc_opts -PmainConfigFile='$main_config_file' --warning-mode=none"
