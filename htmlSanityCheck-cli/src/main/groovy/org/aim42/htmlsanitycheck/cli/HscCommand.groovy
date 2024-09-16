38c287cc16ebcdc9e7c547a3bdbfe2a00e0fada9
Gerd Aschemann <gerd@aschemann.net>
2024-09-16 12:39:22 +0200

    WIP 318 Explicitly set logger for CLI

--- htmlSanityCheck-cli/src/main/groovy/org/aim42/htmlsanitycheck/cli/HscCommand.groovy
+++ htmlSanityCheck-cli/src/main/groovy/org/aim42/htmlsanitycheck/cli/HscCommand.groovy
@@ -4,9 +4,6 @@ import org.aim42.htmlsanitycheck.AllChecksRunner
 import org.aim42.htmlsanitycheck.Configuration
 import org.aim42.htmlsanitycheck.ProductInformation
 import org.aim42.htmlsanitycheck.check.AllCheckers
-import org.apache.log4j.Level
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import picocli.CommandLine
 import picocli.CommandLine.Command
 import picocli.CommandLine.Option
@@ -14,6 +11,13 @@ import picocli.CommandLine.Parameters
 
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.util.logging.Formatter
+import java.util.logging.ConsoleHandler
+import java.util.logging.Level
+import java.util.logging.LogManager
+import java.util.logging.LogRecord
+import java.util.logging.Logger
+import java.util.logging.SimpleFormatter
 
 // see end-of-file for license information
 
@@ -22,7 +26,44 @@ import java.nio.file.Paths
         showDefaultValues = true
 )
 class HscCommand implements Runnable {
-    private static final Logger logger = LoggerFactory.getLogger(HscCommand.class)
+    private static final Logger rootLogger
+    private static ConsoleHandler consoleHandler
+
+    private static final Logger logger
+
+    private static final String SHORT_FORMAT = "%1\$s%n"
+    private static final String LEVEL_FORMAT = "%2\$-7s - %1\$s%n"
+    private static final String LEVEL_CLASS_FORMAT = "%2\$-7s - %3\$s - %1\$s%n"
+
+    private static void setLogFormatter(final String format) {
+        Formatter formatter = new SimpleFormatter() {
+
+            @Override
+            synchronized String format(LogRecord lr) {
+                return String.format(format,
+                        lr.getMessage(),
+                        lr.getLevel().getLocalizedName(),
+                        String.format("%-30s", lr.getSourceClassName())
+                )
+            }
+        }
+        consoleHandler.setFormatter(formatter)
+    }
+
+    static {
+        LogManager logManager = LogManager.getLogManager();
+        logManager.reset();
+
+        rootLogger = Logger.getLogger("org.aim42.htmlsanitycheck")
+        logger = Logger.getLogger(HscCommand.class.name)
+
+        rootLogger.setLevel(Level.WARNING)
+        consoleHandler = new ConsoleHandler()
+
+        setLogFormatter(SHORT_FORMAT)
+
+        rootLogger.addHandler(consoleHandler)
+    }
 
     HscRunner runner
 
@@ -33,7 +74,7 @@ class HscCommand implements Runnable {
     @Option(names = ["-v", "--verbose"],
             description = "Increase verbosity. Repeatable, cumulative -v -vv -vvv",
             fallbackValue = "true", arity = "0..3")
-    private boolean[] verbosity;
+    private boolean[] verbosity
 
     @Option(names = ["-V", "--version"], description = "Display version information")
     boolean versionRequested
@@ -77,39 +118,43 @@ class HscCommand implements Runnable {
         CommandLine cmd
 
         private static void setLogLevel(final Level level) {
-            org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger("org.aim42.htmlsanitycheck"
-            );
-            logger.setLevel(level);
+            rootLogger.setLevel(level)
+            consoleHandler.setLevel(level)
         }
 
         private void configureLogging() {
             if (hscCommand.verbosity != null) {
-                int verbosityLevel = hscCommand.verbosity.length;
+                int verbosityLevel = hscCommand.verbosity.length
                 switch (verbosityLevel) {
                     case 0:
                         // Default logging level
-                        break;
+                        break
                     case 1:
-                        setLogLevel(Level.INFO);
-                        break;
+                        setLogLevel(Level.INFO)
+                        break
                     case 2:
-                        setLogLevel(Level.DEBUG);
-                        break;
+                        setLogLevel(Level.FINE)
+                        setLogFormatter(LEVEL_FORMAT)
+                        break
                     case 3:
-                    default:
-                        setLogLevel(Level.TRACE);
-                        break;
+                        setLogLevel(Level.FINER)
+                        setLogFormatter(LEVEL_CLASS_FORMAT)
+                        break
+                    case 4:
+                        setLogLevel(Level.FINEST)
+                        setLogFormatter(LEVEL_CLASS_FORMAT)
+                        break
                 }
             }
         }
 
         void run() {
-            configureLogging();
             if (hscCommand.versionRequested) {
                 System.out.println("Version: ${ProductInformation.VERSION}")
                 return
             }
 
+            configureLogging()
             def srcDocuments = hscCommand.srcDocs ?: hscCommand.findFiles()
             if (!srcDocuments) {
                 System.err.println("Please specify at least one src document (either explicitly or implicitly)")
@@ -128,15 +173,14 @@ class HscCommand implements Runnable {
             // if we have no valid configuration, abort with exception
             configuration.validate()
             logger.fine("Starting HTML sanity check")
-            logger.info("Starting HTML sanity check");
-                // create output directory for checking results
-                resultsDirectory.mkdirs()
+            // create output directory for checking results
+            resultsDirectory.mkdirs()
 
             // create an AllChecksRunner...
             var allChecksRunner = new AllChecksRunner(configuration)
 
             // ... and perform the actual checks
-            var allChecks = allChecksRunner.performAllChecks()
+            var allChecks = allChecksRunner.performAllChecks(false)
 
             // check for findings and fail build if requested
             var nrOfFindingsOnAllPages = allChecks.nrOfFindingsOnAllPages()
