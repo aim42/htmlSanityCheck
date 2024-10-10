1a36ee642fed3a8e7cc59c73e2bb2d984d85f05b
rehan <rehanchalanaprsnl@gmail.com>
2024-10-10 17:02:48 +0530

    replaced test URLs to URLs that use WireMock
    - add hostnames to CustomHostNameResolver
    
    - renamed testing.json to mappings.json
    
    - add mappings for all the tests in mappings.json
    
    Signed-off-by: rehan <rehanchalanaprsnl@gmail.com>

--- htmlSanityCheck-core/src/test/java/org/aim42/htmlsanitycheck/test/dns/CustomHostNameResolver.java
+++ htmlSanityCheck-core/src/test/java/org/aim42/htmlsanitycheck/test/dns/CustomHostNameResolver.java
@@ -3,11 +3,22 @@ package org.aim42.htmlsanitycheck.test.dns;
 import java.lang.reflect.Field;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.List;
 
 public class CustomHostNameResolver {
 
-    public static final String WIREMOCK_HOST = "my.custom.mocked.host";
     private static Object originalResolver;
+
+    private static final List<String> hostnames = Arrays.asList(
+            "www.amazon.com",
+            "google.com",
+            "junit.org",
+            "plumelib.org",
+            "people.csail.mit.edu",
+            "arc42.org",
+            "mock.codes"
+    );
 
     static {
         try {
@@ -21,7 +32,7 @@ public class CustomHostNameResolver {
 
     public InetAddress[] resolve(String hostname) throws UnknownHostException {
         // Custom DNS resolution logic
-        if (WIREMOCK_HOST.equals(hostname)) {
+        if (hostnames.contains(hostname)) {
             return new InetAddress[]{InetAddress.getByAddress("localhost", new byte[]{127, 0, 0, 1})};
         }
         // Fallback to original resolver using reflection
