b2a1fa9f54b2e70a5a1d63798aaae2c85135e728
rehan <rehanchalanaprsnl@gmail.com>
2024-10-11 13:33:59 +0530

    feat: implement CustomHostNameResolver for custom DNS resolution in tests
    - progress towards fixing #331

--- htmlSanityCheck-core/src/test/groovy/org/aim42/htmlsanitycheck/check/BrokenHttpLinksCheckerSpec.groovy
+++ htmlSanityCheck-core/src/test/groovy/org/aim42/htmlsanitycheck/check/BrokenHttpLinksCheckerSpec.groovy
@@ -4,13 +4,17 @@ import org.aim42.htmlsanitycheck.Configuration
 import org.aim42.htmlsanitycheck.collect.SingleCheckResults
 import org.aim42.htmlsanitycheck.html.HtmlConst
 import org.aim42.htmlsanitycheck.html.HtmlPage
-import org.aim42.htmlsanitycheck.tools.Web
+import org.aim42.htmlsanitycheck.test.dns.CustomHostNameResolver
 import org.wiremock.integrations.testcontainers.WireMockContainer
 import spock.lang.Ignore
 import spock.lang.IgnoreIf
 import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Unroll
+
+import java.lang.reflect.Field
+import java.lang.reflect.Proxy
+
 
 // see end-of-file for license information
 
@@ -29,19 +33,17 @@ class BrokenHttpLinksCheckerSpec extends Specification {
             .withMappingFromResource("mappings.json")
             .withExposedPorts(8080)
 
+    @Shared
+    CustomHostNameResolver customHostNameResolver = new CustomHostNameResolver()
+
     /** executed once before all specs are executed **/
     def setupSpec() {
         wireMockServer.start()
         port = wireMockServer.getMappedPort(8080)
-    }
-
-    /** executed once after all specs are executed **/
-    def cleanupSpec() {
-        wireMockServer.stop()
+        registerCustomDnsResolver()
     }
 
     /* executed before every single spec */
-
     def setup() {
         myConfig = new Configuration()
         brokenHttpLinksChecker = new BrokenHttpLinksChecker( myConfig )
@@ -49,6 +51,36 @@ class BrokenHttpLinksCheckerSpec extends Specification {
         collector = new SingleCheckResults()
     }
 
+
+    /** executed once after all specs are executed **/
+    def cleanupSpec() {
+        wireMockServer.stop()
+    }
+
+
+    // Custom method to register the DNS resolver
+    private void registerCustomDnsResolver() {
+        try {
+            Field implField = InetAddress.class.getDeclaredField("impl");
+            implField.setAccessible(true);
+            Object currentImpl = implField.get(null);
+
+            Proxy newImpl = (Proxy) Proxy.newProxyInstance(
+                    currentImpl.getClass().getClassLoader(),
+                    currentImpl.getClass().getInterfaces(),
+                    (proxy, method, args) -> {
+                        if ("lookupAllHostAddr".equals(method.getName()) && args.length == 1 && args[0] instanceof String) {
+                            return customHostNameResolver.resolve((String) args[0]);
+                        }
+                        return method.invoke(currentImpl, args);
+                    }
+            );
+
+            implField.set(null, newImpl);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to register custom DNS resolver", e);
+        }
+    }
     /**
      * checking for internet connectivity is a somewhat brittle - as there's no such thing as "the internet"
      * (the checker will most likely use google.com as a proxy for "internet"
@@ -80,7 +112,7 @@ class BrokenHttpLinksCheckerSpec extends Specification {
     def "one syntactically correct http URL is ok"() {
         given: "an HTML page with a single correct anchor/link"
         String HTML = """$HtmlConst.HTML_HEAD 
-                <a href="http://localhost:$port/google">google</a>
+                <a href="http://${CustomHostNameResolver.WIREMOCK_HOST}:$port/google">google</a>
                 $HtmlConst.HTML_END """
 
         htmlPage = new HtmlPage(HTML)
