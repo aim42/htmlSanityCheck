4dd147bb4f3e48f0349cedc1a06aa93b1985e4d3
Thomas Ruhroth <thomas.ruhroth@msg.group>
2024-10-16 07:19:02 +0200

    #314 Add JaCoCo for Maven plugin

--- htmlSanityCheck-maven-plugin/src/test/java/org/aim42/htmlSanityCheck/htmlsanitycheckmavenplugin/HtmlSanityCheckMojoTest.java
+++ htmlSanityCheck-maven-plugin/src/test/java/org/aim42/htmlSanityCheck/htmlsanitycheckmavenplugin/HtmlSanityCheckMojoTest.java
@@ -1,12 +1,20 @@
 package org.aim42.htmlSanityCheck.htmlsanitycheckmavenplugin;
 
+
+import org.aim42.htmlsanitycheck.Configuration;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 
 class HtmlSanityCheckMojoTest {
 
 
     @Test
     void setupConfiguration() {
-        return;
+        HtmlSanityCheckMojo mojo = new HtmlSanityCheckMojo();
+        Configuration config = mojo.setupConfiguration();
+        Assertions.assertThat(config).isNotNull();
+
     }
 }
