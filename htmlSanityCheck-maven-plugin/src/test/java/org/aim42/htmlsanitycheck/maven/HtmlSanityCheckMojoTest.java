761df53331686a3c573785d4d76dac83d8e36a0c
Gerd Aschemann <gerd@aschemann.net>
2024-11-20 09:40:33 +0100

    CAUTION NOT WORKING!
    Unified test cases from conflicting rebase

--- htmlSanityCheck-maven-plugin/src/test/java/org/aim42/htmlsanitycheck/maven/HtmlSanityCheckMojoTest.java
+++ htmlSanityCheck-maven-plugin/src/test/java/org/aim42/htmlsanitycheck/maven/HtmlSanityCheckMojoTest.java
@@ -1,20 +1,15 @@
-package org.aim42.htmlSanityCheck.htmlsanitycheckmavenplugin;
-
+package org.aim42.htmlsanitycheck.maven;
 
 import org.aim42.htmlsanitycheck.Configuration;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
 class HtmlSanityCheckMojoTest {
-
-
     @Test
     void setupConfiguration() {
         HtmlSanityCheckMojo mojo = new HtmlSanityCheckMojo();
         Configuration config = mojo.setupConfiguration();
         Assertions.assertThat(config).isNotNull();
-
     }
 }
